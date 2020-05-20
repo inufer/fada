fea18958d8e2cbe894cce144fc0b1e18d909723c
Juan C Rosero <57236205+JuanK120@users.noreply.github.com>
2020-05-20 18:16:06 -0500

    Update main.java

--- main.java
+++ main.java
@@ -33,31 +33,36 @@ public class main {
 		
 		System.out.print("\n \n");
 		   
-		n2 = 9;
-		m2 = 4;
-		k2 = 3;
-		animales2 = {leon, panteranegra, cebra, cocodrilo, boa, loro, caiman, tigre, capibara};
-		grandezas2 = {9, 7, 6, 5, 4, 2, 3, 8, 1};
-		apertura2 = {{caiman, capibara, loro}, {boa, caiman, capibara}, {cocodrilo, capibara, loro}, {panteranegra, cocodrilo, loro}, {tigre, loro, capibara}, {leon, caiman, loro}, {leon, cocodrilo, boa}, {leon, panteranegra, cebra}, {tigre, cebra, panteranegra}};
-		partes2 = {{{caiman, capibara, loro}, {tigre, loro, capibara}, {tigre, cebra, panteranegra}},
-			{{panteranegra, cocodrilo, loro}, {leon, panteranegra, cebra}, {cocodrilo, capibara, loro}},
-			{{boa, caiman, capibara}, {leon, caiman, loro}, {leon, cocodrilo, boa}}};
+		int n2 = 9;
+		int m2 = 4;
+		int k2 = 3;
+		String[] animales2 = {"leon", "panteranegra", "cebra", "cocodrilo", "boa", "loro", "caiman", "tigre", "capibara"};
+		int[] grandezas2 = {9, 7, 6, 5, 4, 2, 3, 8, 1};
+		String[][] apertura2 = {{"caiman", "capibara", "loro"}, {"boa", "caiman", "capibara"}, {"cocodrilo", "capibara", "loro"},
+								{"panteranegra", "cocodrilo", "loro"}, {"tigre", "loro", "capibara"}, {"leon", "caiman", "loro"},
+								{"leon", "cocodrilo", "boa"}, {"leon", "panteranegra", "cebra"}, {"tigre", "cebra", "panteranegra"}};
+		String[][][] partes2 = {{{"caiman", "capibara", "loro"}, {"tigre", "loro", "capibara"}, {"tigre", "cebra", "panteranegra"}},
+								{{"panteranegra", "cocodrilo", "loro"}, {"leon", "panteranegra", "cebra"}, {"cocodrilo", "capibara", "loro"}},
+								{{"boa", "caiman", "capibara"}, {"leon", "caiman", "loro"}, {"leon", "cocodrilo", "boa"}}};
 		
 		System.out.print("prueba con complejidad n \n");
 		
-		O_n p1= new O_n(n2,m2,k2,animales2,grandezas2,apertura2,partes2);
+		O_n p4= new O_n(n2,m2,k2,animales2,grandezas2,apertura2,partes2);
 		
 		System.out.print("\n \n");
 		
 		System.out.print("prueba con complejidad n2 \n");
 		
-		O_n2 p1= new O_n2(n2,m2,k2,animales2,grandezas2,apertura2,partes2);
+		O_n2 p5= new O_n2(n2,m2,k2,animales2,grandezas2,apertura2,partes2);
 		
 		System.out.print("\n \n");
 		
 		System.out.print("prueba con complejidad nlogn \n");
 		
+		O_nlogn p6 = new O_nlogn(n2,m2,k2,animales2,grandezas2,apertura2,partes2);
+		
 		System.out.print("\n \n");
+
 
 	}
 
